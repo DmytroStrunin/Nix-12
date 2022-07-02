299e638d0eff8b5db538dd2705bc57bbbf2f8862
Dmytro Strunin <strunin.dmytro@gmail.com>
2022-07-02 14:53:45 +0300

    first commit

---
+++ src/main/java/com/nixsolutions/Main.java
@@ -0,0 +1,13 @@
+package com.nixsolutions;
+
+/**
+ * The {@code Main} class
+ *
+ * @author Strunin Dmytro
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
