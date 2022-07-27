87d888c6b70c39ed03c4893b5b411e85caebb918
Dmytro Srtunin <rimdows@gmai.com>
2022-07-27 17:50:57 +0300

    edit BinaryTree.java add methods

--- src/main/java/com/nixsolutions/utils/BinaryTree.java
+++ src/main/java/com/nixsolutions/utils/BinaryTree.java
@@ -2,6 +2,8 @@ package com.nixsolutions.utils;
 
 import com.nixsolutions.model.Vehicle;
 
+import java.util.Comparator;
+
 
 /**
  * The {@code BinaryTree} class
@@ -10,13 +12,18 @@ import com.nixsolutions.model.Vehicle;
  * @version 1.0
  */
 public class BinaryTree<T extends Vehicle> {
-    private Node head;
+    private final Comparator<T> comparator;
+    private Node root;
     private int size;
+
+    public BinaryTree(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
 
     private class Node {
         private T data;
-        private Node small;
-        private Node large;
+        private Node leftChild;
+        private Node rightChild;
 
         @Override
         public String toString() {
@@ -25,20 +32,33 @@ public class BinaryTree<T extends Vehicle> {
                     '}';
         }
 
-        public Node(T data, Node small, Node large) {
+        public Node(T data) {
             this.data = data;
-            this.small = small;
-            this.large = large;
         }
     }
 
     public void add(T vehicle) {
-        if (head == null) {
-            head = new Node(vehicle, null, null);
+        if (root == null) {
+            root = new Node(vehicle);
         } else {
-            head = new Node(vehicle, null, head);
-            head.large.small = head;
+            buildNode(root, vehicle);
         }
         size++;
+    }
+
+    private void buildNode(Node node, T vehicle) {
+        if (comparator.compare(vehicle, node.data) < 0) {
+            if (node.leftChild != null) {
+                buildNode(node.leftChild, vehicle);
+            } else {
+                node.leftChild = new Node(vehicle);
+            }
+        } else if (comparator.compare(vehicle, node.data) > 0) {
+            if (node.rightChild != null) {
+                buildNode(node.rightChild, vehicle);
+            } else {
+                node.rightChild = new Node(vehicle);
+            }
+        }
     }
 }
